db9e639d12d86604df1cb7692f586ea2ae20d2b7
Diego <diegonarutoromero@hotmail.com>
2021-03-22 13:10:31 -0500

    jugando con js

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,10 @@
-var x; 
+var x;
+var y; 
 function mostrar() {
   document.write("se ejecuto la funcion" + "<br />");
 }
 x = 5;
+y = 4;
 if(x == 5) {
   
   mostrar()
@@ -10,4 +12,7 @@ if(x == 5) {
 
 if(x > 3) {
   document.write("otra cosa");
-} 
+}
+if(y < x){
+  alert(se ejecuto la variable Y sin probremas);
+}
