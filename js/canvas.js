f890e44ae59cfae60a2cb4642997cfeaf889ca12
Diego <diegonarutoromero@hotmail.com>
2021-03-22 20:29:49 -0500

    final

--- js/canvas.js
+++ js/canvas.js
@@ -4,8 +4,15 @@ document.addEventListener("keyup", oprimirTeclas);
 var x = 250;
 var y = 250;
 
+teclas = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
 dibujarLinea("white", x - 1, y - 1, x + 2, y + 2, contexto);
-function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lienzo){
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lienzo) {
   lienzo.beginPath();
   lienzo.strokeStyle = color;
   lienzo.lineWidth = 3;
@@ -15,6 +22,26 @@ function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lienzo){
   lienzo.closePath();
 }
 
-function oprimirTeclas(evento){
-  console.log(evento);
+function oprimirTeclas(evento) {
+  var colorcito = "white";
+  var movimiento = 10;
+  switch (evento.keyCode) {
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, contexto);
+      x = x - movimiento;
+      break;
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, contexto);
+      y = y - movimiento;
+      break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, contexto);
+      x = x + movimiento;
+      break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, contexto);
+      y = y + movimiento;
+      break;
+  }
+  console.log(evento.keyCode);
 }
